3623f8ccdc2022faac1cba6abe85b9d6e75a2683
Charan <charanxmn@gmail.com>
2024-10-02 23:10:44 +0530

    Added the custom theme to daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,34 @@ const config: Config = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        gxtheme: {
+          primary: "#2563eb",
+          "primary-content": "#f3f4f6",
+          secondary: "#f5f5f4",
+          "secondary-content": "#160016",
+          accent: "#a855f7",
+          "accent-content": "#f5f5f4",
+          neutral: "#f3f4f6",
+          "neutral-content": "#160016",
+          "base-100": "#ffffff",
+          "base-200": "#dedede",
+          "base-300": "#bebebe",
+          "base-content": "#161616",
+          info: "#bae6fd",
+          "info-content": "#292524",
+          success: "#86efac",
+          "success-content": "#001616",
+          warning: "#fef08a",
+          "warning-content": "#001600",
+          error: "#fca5a5",
+          "error-content": "#160000",
+        },
+      },
+    ],
+  },
   plugins: [daisyui],
 };
 export default config;
