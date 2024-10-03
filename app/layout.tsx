33d5b868488b9da1d62012a1c73348a1671b9aea
Charan <charanxmn@gmail.com>
2024-10-04 03:48:34 +0530

    repo all set for fast prototyping
    - added shadcn.
    - setup dark and light themes.

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export const metadata: Metadata = {
+  title: "ANTF-AP",
+  description:
+    "Awareness events platform for the esteemed ANTF of Andhra Pradesh",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
